a76e4ad694d33156793f4ef77d1de4b08e7675d6
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2023-11-08 20:25:22 +0530

    banner section checkin

--- amazon-clone-main-project/src/components/Home/Banner.js
+++ amazon-clone-main-project/src/components/Home/Banner.js
@@ -1,9 +1,92 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Slider from "react-slick";
+import {
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  bannerImgFour,
+  bannerImgFive,
+}from "../../assets/index";
 const Banner = () => {
+  const[doActie,setDoActive]=useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange:(prev,next)=>{
+      setDoActive(next);
+    },
+    appendDots: dots => (
+      <div
+        style={{
+          position:"absolute",
+          top:"70%",
+          left:"45%",
+          transform:"translate(50% -50%)",
+          width:"210px",
+        }}
+      >
+        <ul style={{width:"100%",display:"flex",alignItems:"center",justifyContent:"space-between"}}> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div
+        style={
+          i === doActie ? {
+            width:"30px",
+            height:"30px",
+            borderRadius:"50%",
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            cursor:"pointer",
+            color:"#ffffff",
+            background:"#131921",
+            padding:"8px 0",
+            border:"1px solid #f3a847"
+          }:{
+            width:"30px",
+            height:"30px",
+            borderRadius:"50%",
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            cursor:"pointer",
+            color:"#ffffff",
+            background:"#232F3E",
+            padding:"8px 0",
+            border:"1px solid white"
+          }
+        }
+      >
+        {i + 1}
+      </div>
+    )
+  };
   return (
-    <div>
-      
+    <div className='w-full'>
+      <div className='w-full h-full relative'>
+        <Slider {...settings}>
+          <div>
+            <img src={bannerImgOne} alt="bannerImgOne" />
+          </div>
+          <div>
+            <img src={bannerImgTwo} alt="bannerImgTwo" />
+          </div>
+          <div>
+            <img src={bannerImgThree} alt="bannerImgThree" />
+          </div>
+          <div>
+            <img src={bannerImgFour} alt="bannerImgFour" />
+          </div>
+          <div>
+            <img src={bannerImgFive} alt="bannerImgFive" />
+          </div>
+        </Slider>
+      </div>
     </div>
   )
 }
